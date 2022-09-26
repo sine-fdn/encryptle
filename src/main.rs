7fa188a912c2c5d7ad92f64550554a7f83d8e6b4
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-09-26 11:29:42 +0100

    fix: random word choice per day

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,31 @@ use rand::SeedableRng;
 #[macro_use]
 extern crate rocket;
 
+#[launch]
+fn rocket() -> _ {
+    let words: Vec<&str> = include_str!("words.txt").trim().split('\n').collect();
+
+    let prg = check_program(include_str!("wordle.garble.rs")).unwrap();
+    let circuit = compile_program(&prg, &"wordle").unwrap();
+
+    let mut nonce_rng = StdRng::from_entropy();
+    let nonce: u64 = nonce_rng.gen();
+
+    let handler = move |_: MpcRequest| -> Result<(Circuit, Vec<bool>), String> {
+        let current_date = Utc::today();
+        let days_since_unix_epoch = current_date.num_days_from_ce();
+
+        let random_choice = (days_since_unix_epoch as u64 + nonce) as usize % words.len();
+        println!("Secret word: {}", words[random_choice]);
+
+        let input = word_as_garble_literal(words[random_choice]);
+        let input = serialize_input(Role::Contributor, &prg, &circuit.fn_def, &input)?;
+        Ok((circuit.gates.clone(), input))
+    };
+
+    build(Box::new(handler))
+}
+
 fn word_as_garble_literal(word: &str) -> String {
     let word = word.trim();
     if word.len() != 5 {
@@ -31,39 +56,4 @@ fn word_as_garble_literal(word: &str) -> String {
     }
     input_as_garble_literal += "]";
     input_as_garble_literal
-}
-
-#[launch]
-fn rocket() -> _ {
-    let words: Vec<&str> = include_str!("words.txt").trim().split('\n').collect();
-    println!("{:?}", words);
-
-    let prg = check_program(include_str!("wordle.garble.rs")).unwrap();
-    let circuit = compile_program(&prg, &"wordle").unwrap();
-
-    let current_date = Utc::today();
-    let days_since = current_date.num_days_from_ce();
-
-    let mut first_rng = StdRng::seed_from_u64(days_since as u64);
-    let first_random_number = first_rng.gen::<u64>();
-    println!("{first_random_number}");
-
-    let mut second_rng = StdRng::seed_from_u64(first_random_number);
-    let second_random_number = second_rng.gen::<u64>();
-    println!("{second_random_number}");
-
-    let random_index = second_random_number as usize % words.len();
-    println!("{}", words[random_index]);
-
-    let handler = move |_: MpcRequest| -> Result<(Circuit, Vec<bool>), String> {
-        let input = serialize_input(
-            Role::Contributor,
-            &prg,
-            &circuit.fn_def,
-            &word_as_garble_literal(words[random_index]),
-        )?;
-        Ok((circuit.gates.clone(), input))
-    };
-
-    build(Box::new(handler))
 }
